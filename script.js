2d6862abaa3d2ac64aaf4174e2a0682565f49a7e
cs-ana <anaaclaraacosta776@gmail.com>
2025-06-05 15:56:36 -0300

    Atualizar o imagens/

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-function selectMeal(meal) {
-    document.getElementById('selectedMeal').innerHTML = 
-      `<h2>Você selecionou o ${meal}!</h2>`;
-}
-
-function clearSelection() {
-    document.getElementById('selectedMeal').innerHTML = '';
-}
+foto1.jpg
+foto2.jpg
+foto3.jpg
+foto4.jpg
+foto5.jpg
+foto6.jpg
